3cf9f1fdf8a25b34a80ae4ddce006ffbc9fc75f6
Ayush Sonawale <75654165+ayush2511@users.noreply.github.com>
2021-02-12 12:43:35 +0530

    Add files via upload

---
+++ PositiveOrNegative.c
@@ -0,0 +1,21 @@
+//Check Positive Or Negative
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the Number: ");
+  scanf("%d",&n);
+  if(n>0)
+  {
+    printf("Number Is positive\n");
+   
+  }
+  else
+  {
+    printf("Number is negative\n");
+    
+  }
+  
+
+
+}
