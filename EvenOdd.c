696af53a6eb0053f8cdff541be6b2011d4c41361
Ayush Sonawale <75654165+ayush2511@users.noreply.github.com>
2021-02-12 12:41:15 +0530

    Add files via upload

---
+++ EvenOdd.c
@@ -0,0 +1,19 @@
+//Check Even or Odd
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the Number: ");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("number is Even\n");
+  }
+    else
+    {
+    printf("number is Odd\n");
+    }
+  
+
+
+}
