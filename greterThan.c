5cd703147e60514d5a567990804b77f5b5e97e26
Ayush Sonawale <75654165+ayush2511@users.noreply.github.com>
2021-02-12 12:41:44 +0530

    Add files via upload

---
+++ greterThan.c
@@ -0,0 +1,20 @@
+//Check Who is greater 
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter the number: ");
+ scanf("%d",&a);
+ printf("Enter the number: ");
+ scanf("%d",&b);
+ if(a>b)
+ {
+  printf("The number is Greter than b\n");
+ }
+ else
+ {
+  printf("The number is greter than a\n");
+ }
+
+}
+
