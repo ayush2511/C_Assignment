e8792b19ee5499a314ffe8928f4642a1c344677a
Ayush Sonawale <75654165+ayush2511@users.noreply.github.com>
2021-02-12 12:44:32 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,14 @@
+//Write C program for  Addition
+#include<stdio.h>
+int main()
+{
+	int a, b,c;
+	printf("\nEnter The FIrst Number: ",a);
+	scanf("%d",&a);
+    printf("\nEnter The Second Number: ",b);
+	scanf("%d",&b);	
+	c=a+b;
+	printf("\nTotal value of Sum: %d\n",c);
+	return 0;
+}
+	
